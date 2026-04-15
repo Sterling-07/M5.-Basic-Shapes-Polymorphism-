997a10fe798b1915ab9312852259030716382bfd
sterl <sterlingrodgers2007@gmail.com>
2026-04-14 20:18:09 -0400

    Create shapes and their values within main, then display the name of each shape and its area.

--- M5. Basic Shapes (Polymorphism).cpp
+++ M5. Basic Shapes (Polymorphism).cpp
@@ -9,6 +9,16 @@ using namespace std;
 
 int main()
 {
+	BasicShape* shape[3];
 
+	shape[0] = new Circle(1, 3, 7);
+	shape[1] = new Rectangle(5, 6);
+	shape[2] = new Square(7);
+
+	cout << shape[0]->getName() << endl << "Area = " << shape[0]->getArea() << endl;
+	cout << shape[1]->getName() << endl << "Area = " << shape[1]->getArea() << endl;
+	cout << shape[2]->getName() << endl << "Area = " << shape[2]->getArea() << endl;
+
+	return 0;
 }
 
